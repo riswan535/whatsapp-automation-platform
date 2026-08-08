8d2182d31cd4eb58e693179773e209a9df28108b
riswan535 <riswanvariswanva6673@gmail.com>
2026-08-08 16:14:45 +0530

    Configure frontend API URL

--- frontend/automation/src/api/axios.js
+++ frontend/automation/src/api/axios.js
@@ -1,7 +1,9 @@
+//---------------------------------------------------------BASE API-----------------------------------------------------------------------------------------------------------
+
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default api;
